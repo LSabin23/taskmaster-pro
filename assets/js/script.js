28fc4d18b713fc74b8fb7041e12d08f642d2ac80
unknown <44147809+LSabin23@users.noreply.github.com>
2021-10-28 18:09:09 -0400

    added datepicker to modal and edited tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -100,12 +100,20 @@ $('.list-group').on('click', 'span', function () {
     .val(date)
   // swap out elements
   $(this).replaceWith(dateInput)
-  // automatically focus on new element
+  // enable jQuery datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      // when calendar is closed, force a 'change' event on the 'dateInput'
+      $(this).trigger('change')
+    }
+  })
+  // automatically bring up the calendar
   dateInput.trigger('focus')
 })
 
 // value of due date was changed
-$('.list-group').on('blur', 'input[type=\'text\']', function () {
+$('.list-group').on('change', 'input[type=\'text\']', function () {
   // get current text
   var date = $(this)
     .val()
@@ -163,6 +171,10 @@ $("#task-form-modal .btn-primary").click(function () {
     saveTasks();
   }
 });
+
+$('#modalDueDate').datepicker({
+  minDate: 1
+})
 
 // remove all tasks
 $("#remove-tasks").on("click", function () {
